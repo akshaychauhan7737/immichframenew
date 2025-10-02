3849ec9d315d2ebc131b92ee8140c4724c6b64ec
Akshay Chauhan <king.chauhan25@gmail.com>
2025-10-02 07:08:45 +0000

    url" variable slideshow-1  | nginx: [emerg] unknown "next_public_immich_

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Substitute environment variables in the Nginx config template
-envsubst '${NEXT_PUBLIC_IMMICH_API_URL} ${NEXT_PUBLIC_IMMICH_API_KEY} ${NEXT_PUBLIC_LATITUDE} ${NEXT_PUBLIC_LONGITUDE} ${OPENWEATHER_API_KEY}' < /app/nginx.conf > /etc/nginx/conf.d/default.conf
+envsubst '${NEXT_PUBLIC_IMMICH_API_URL} ${NEXT_PUBLIC_IMMICH_API_KEY} ${NEXT_PUBLIC_LATITUDE} ${NEXT_PUBLIC_LONGITUDE} ${OPENWEATHER_API_KEY}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Ensure log files exist and have correct permissions
 touch /var/log/nginx/access.log /var/log/nginx/error.log
